5b965bbbd47ee7968a931c63709a49f805109a1e
Kirua <yo44prg@icloud.com>
2021-09-16 11:14:00 +0200

    feat: add cross-origin for picture controller

--- src/main/java/com/kirua/galactic/controller/GalacticPictureController.java
+++ src/main/java/com/kirua/galactic/controller/GalacticPictureController.java
@@ -22,6 +22,7 @@ import java.security.Principal;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @AllArgsConstructor
 @RequestMapping("/picture")
